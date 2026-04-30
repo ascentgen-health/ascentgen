8312e8576da2afa87d7be3ead22ee74e2bd42f50
ascendgen-health <info@ascentgen.health>
2026-04-30 12:51:57 -0700

    chore: remove unused web app

--- apps/web/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Ascendgen marketing test</h1>
-      <p>If you see this at ascentgen.health, we’re hitting apps/web/src/app/page.tsx.</p>
-    </main>
-  );
-}
